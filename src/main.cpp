ac7fe7378aa65abdb33fe963db8a8f390d4d0bac
Ben Scholar <scholar@case.edu>
2022-12-16 19:33:44 -0500

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -18,27 +18,39 @@
 
 #include "fort.hpp"
 
+// namespace declarations
 namespace fs = std::filesystem;
 
+// typedefs
 template <typename T, typename U> using map = std::map<T, U>;
 template <typename T> using set = std::set<T>;
-
 using proc_map = std::unordered_map<uint32_t, std::string>;
 
+// method for finding list of all valid pids
 std::vector<uint32_t> get_pid_list();
+
+// processes PID by analyzing /proc/xxxx/maps
+// adds entries to maps
 void process_pid(uint32_t pid, map<uint32_t, set<std::string>> &proc_map,
                  map<std::string, set<uint32_t>> &obj_map,
                  ::proc_map &proc_names);
 
+// follows symlink until it is no longer a symlink
 void follow_symlink(fs::path &path);
 
+// limits a string to `linewidth` characters per line
+// inserts newline character after `linewidth` characters
 std::string wrap_string(const std::string &s, size_t line_width);
 
+// print out info by process
 void print_proc(map<uint32_t, set<std::string>> &m, proc_map &proc_names,
                 unsigned short width);
+
+// print out info by shared object
 void print_obj(map<std::string, set<uint32_t>> &m, proc_map &proc_names,
                unsigned short width);
 
+// prints help for application
 void print_usage() {
   std::cout << "Usage: shared_info < -obj | -proc >" << std::endl;
   std::cout << "-proc -- sort by process" << std::endl;
@@ -49,6 +61,7 @@ int main(int argc, char **argv) {
   argc--;
   argv++;
 
+  // need at least 1 input arg to run correctly
   if (argc == 0) {
     print_usage();
     return 1;
@@ -72,15 +85,19 @@ int main(int argc, char **argv) {
   map<std::string, set<uint32_t>> obj2proc;
   proc_map proc_names;
 
+  // aquire list of pids
   const auto pid_list = get_pid_list();
 
+  // process every available pid
   for (const auto pid : pid_list) {
     process_pid(pid, proc2obj, obj2proc, proc_names);
   }
 
+  // get current terminal size in characters
   struct winsize w;
   ioctl(0, TIOCGWINSZ, &w);
 
+  // print out data in respective format
   if (sort_by_process) {
     print_proc(proc2obj, proc_names, w.ws_col);
   } else {
@@ -90,6 +107,10 @@ int main(int argc, char **argv) {
   return 0;
 }
 
+// aquires a list of every accessible pid from /proc/n/maps
+// basically loops over every subdirectory of /proc and checks whether the
+// name of the folder is strictly numeric
+// returns vector of pids
 std::vector<uint32_t> get_pid_list() {
   using dir_iter = std::filesystem::directory_iterator;
 
@@ -112,20 +133,27 @@ std::vector<uint32_t> get_pid_list() {
 void process_pid(uint32_t pid, map<uint32_t, set<std::string>> &proc_map,
                  map<std::string, set<uint32_t>> &obj_map,
                  ::proc_map &proc_names) {
+
+  // get correct filepath to /proc/pid/maps
   std::string path("/proc/");
   path.append(std::to_string(pid));
   path.append("/maps");
 
+  // open file stream
   std::ifstream in_file(path);
   if (in_file.bad()) {
     std::cerr << "Could not open file: " << path << std::endl;
     return;
   }
 
+  // boolean denoting whether the program has found the name of the application
+  // corresponding to the current pid
   bool found_name = false;
 
   char so_name[1024];
+  // loop over every line in maps file
   for (std::string line; std::getline(in_file, line);) {
+    // extract all data from line
     std::sscanf(line.c_str(), "%*llx-%*llx %*s %*llx %*lld:%*lld %*lld %s",
                 so_name);
 
@@ -139,9 +167,12 @@ void process_pid(uint32_t pid, map<uint32_t, set<std::string>> &proc_map,
       follow_symlink(so_path);
       so_str = so_path.string();
 
+      // insert new set into proc_map if this entry
+      // does not exist yet
       if (!proc_map.contains(pid)) {
         proc_map.insert({pid, set<std::string>()});
       }
+      // insert so
       proc_map.at(pid).insert(so_str);
 
       if (!obj_map.contains(so_str)) {
@@ -217,6 +248,8 @@ void print_obj(map<std::string, set<uint32_t>> &m, proc_map &proc_names,
 }
 
 void follow_symlink(std::filesystem::path &path) {
+  // loop until path is no longer a symlink (pointing at real file or maybe
+  // hardlink?)
   while (1) {
     fs::directory_entry dir_ent(path);
     if (!dir_ent.is_symlink()) {
@@ -234,8 +267,9 @@ std::string wrap_string(const std::string &s, size_t line_width) {
   std::stringstream ss;
 
   int i = 0;
-  int splits = 0;
 
+  // copy string over until we've copied line_width characters over
+  // then add a newline, then continue copying
   for (int i = 0; i < s.size(); ++i) {
     ss << s[i];
     if (i % line_width == 0 && i != 0)
